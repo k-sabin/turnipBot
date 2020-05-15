48e67f3b92c32748504301556ea7657cbfff42b0
Kyle Sabin <kps88@nau.edu>
2020-05-15 11:32:48 -0700

    Added aliases and changed flavor message for commands

--- bot.py
+++ bot.py
@@ -8,18 +8,21 @@ async def on_ready():
     print('Hello World!')
 
 
-@client.command()
+
+#Add to the mention list
+@client.command(aliases=['bought'])
 async def watch(ctx):
     role = discord.utils.get(ctx.guild.roles, name="Stalk Market Trader")
     user = ctx.message.author
     await user.add_roles(role)
-    await ctx.send('Watched!')
+    await ctx.send('Here\'s hoping for a nice surprise!')
 
-@client.command()
+#Remove from the mention list
+@client.command(aliases=['sold'])
 async def unwatch(ctx):
     role = discord.utils.get(ctx.guild.roles, name="Stalk Market Trader")
     user = ctx.message.author
     await user.remove_roles(role)
-    await ctx.send('Unwatched!')
+    await ctx.send('Pleasure doin\' business with ya!')
 
 client.run('NzEwOTA4MjA1NDgzNjg3OTU4.Xr7T6w.MrpU1Jr91UUU5tyYWId9SAgtZzw')
