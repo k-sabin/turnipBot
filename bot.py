be711ea4d5a6d6d966b496932cb50f2f6c5d15bb
Kyle Sabin <kps88@nau.edu>
2020-05-15 11:09:51 -0700

    Set role function

--- bot.py
+++ bot.py
@@ -8,4 +8,12 @@ async def on_ready():
     print('Hello World!')
 
 
+@client.command()
+async def watch(ctx):
+    role = discord.utils.get(ctx.guild.roles, name="Stalk Market Trader")
+    user = ctx.message.author
+    await user.add_roles(role)
+    await ctx.send('Watched!')
+
+
 client.run('NzEwOTA4MjA1NDgzNjg3OTU4.Xr7T6w.MrpU1Jr91UUU5tyYWId9SAgtZzw')
